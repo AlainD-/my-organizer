d678fe00ffc9732c82181ecc0063335edb524da3
Alain D'EURVEILHER <alain.deurveilher@infrabel.be>
2022-10-18 16:51:34 +0200

    Delete unused Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import EventsPage from './pages/EventsPage';
-import Home from './pages/Home';
 
 export default function App() {
   return (
     <div>
-      <Home />
       <EventsPage />
     </div>
   );
