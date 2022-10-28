011860649694a7a08c664116275080894c371c9f
Alain D'EURVEILHER <alain.deurveilher@infrabel.be>
2022-10-28 15:17:42 +0200

    Observe real time the list of events

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -1,5 +1,4 @@
-import { collection, getDocs, QueryDocumentSnapshot, QuerySnapshot, Timestamp } from 'firebase/firestore';
-import { format } from 'path';
+import { collection, DocumentData, onSnapshot, orderBy, query, QueryDocumentSnapshot, QuerySnapshot, Timestamp } from 'firebase/firestore';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { Timeline, TimelineTemplateType } from 'primereact/timeline';
@@ -35,41 +34,20 @@ export default function Events({displayMode}: {displayMode: 'compact'|'large'}) 
     return (
         <Card title={event.title} subTitle={formatDate(event.date)}>
             <p>{event.description}</p>
-            <Button label="Read more" className="p-button-text"></Button>
+            <Button label="..." className="p-button-text p-button-sm"></Button>
         </Card>
     );
   };
 
   useEffect(() => {
-    async function fetchEvents() {
-      const querySnapshot: QuerySnapshot<OrganizerEvent> = await getDocs(collection(firestore, "events")) as QuerySnapshot<OrganizerEvent>;
-      const fetchedEvents: OrganizerEvent[] = querySnapshot.docs.map((doc: QueryDocumentSnapshot<OrganizerEvent>) => {
-        const { date, title, description, icon, colour } = doc.data();
-        const event: OrganizerEvent = {
-          date,
-          title,
-          description,
-          icon,
-          colour
-        };
-        return event;
-      });
-      console.log({fetchedEvents});
+    const unsubscribe = onSnapshot<DocumentData>(query(collection(firestore, "events"), orderBy('date', 'asc')), {includeMetadataChanges: true}, (querySnapshot: QuerySnapshot<DocumentData>) => {
+      const fetchedEvents: OrganizerEvent[] = querySnapshot.docs.map((doc: QueryDocumentSnapshot<DocumentData>) => doc.data() as OrganizerEvent);
       setEvents(() => fetchedEvents);
-    }
+    });
 
-    fetchEvents();
-
-    // setEvents(() => ([
-    //   { title: 'Processing', date: '15/10/2020 14:00', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-calendar', color: '#673AB7' },
-    //   { title: 'Shipped', date: '15/10/2020 16:15', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-building', color: '#FF9800' },
-    //   { title: 'Delivered', date: '16/10/2020 10:00', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-file-edit', color: '#607D8B' },
-    //   { title: 'Delivered', date: '16/10/2020 10:00', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-envelope', color: '#607D8B' },
-    //   { title: 'Shipped', date: '15/10/2020 16:15', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-at', color: '#FF9800' },
-    //   { title: 'Processing', date: '15/10/2020 14:00', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-home', color: '#673AB7' },
-    //   { title: 'Processing', date: '15/10/2020 14:00', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-phone', color: '#673AB7' },
-    //   { title: 'Delivered', date: '16/10/2020 10:00', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', icon: 'pi pi-mobile', color: '#607D8B' },
-    // ]));
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
